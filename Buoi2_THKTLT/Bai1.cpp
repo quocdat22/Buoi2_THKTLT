9f5d77de719db8aa8e99ab5f2b0a62254ded5382
Hoang Le Quoc Dat <hoanglequocdat132002@gmail.com>
2024-07-17 14:32:48 +0700

    Phan2_Bai1_Cau2

--- Buoi2_THKTLT/Bai1.cpp
+++ Buoi2_THKTLT/Bai1.cpp
@@ -1,4 +1,4 @@
-#include<stdio.h>
+﻿#include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 #include "BaiTap.h"
@@ -23,12 +23,25 @@ void xuatMang(int* a, int n) {
 	printf("\n");
 }
 
+//Tạo mảng chứa toàn số chẵn.
+void taoMangChan(int* &a, int &n) {
+	n = 15 + rand() % 16;
+	a = (int*)malloc(n * sizeof(int));
+	for (int i = 0; i < n; i++) {
+		a[i] = (rand()%50) * 2;
+	}
+}
 void bai1() {
 
 	srand(time(NULL));
 	int* a;
 	int n;
-	khoiTaoMangNgauNhien(a, n);
+
+	//Cau 1
+	//khoiTaoMangNgauNhien(a, n);
+
+	//Cau 2
+	taoMangChan(a, n);
 
 	xuatMang(a, n);
 
